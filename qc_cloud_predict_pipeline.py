cb0222399f6a24ab3ecbfbe9723d69845daf67a7
mkoretsky1 <mathew.koretsky1@gmail.com>
2023-05-24 11:09:59 -0400

    naming convention

--- qc_cloud_predict_pipeline.py
+++ qc_cloud_predict_pipeline.py
@@ -490,13 +490,12 @@ for geno, label in zip(cohort_split['paths'], cohort_split['labels']):
     related_dict[label] = related
     
     # het
+    het_out = f'{related_out}_het'
     if related['pass']:
-        het_out = f'{related_out}_het'
         het = het_prune(related_out, het_out)
         het_dict[label] = het
     else:
         related_out = geno
-        het_out = f'{related_out}_het'
         het = het_prune(related_out, het_out)
         het_dict[label] = het
     
